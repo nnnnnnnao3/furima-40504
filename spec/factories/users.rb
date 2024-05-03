a8f24e513b72369e2b99161d1ef86e3219282fdb
nnnnnnnao3 <nnnnnnnao3@gmail.com>
2024-05-03 14:46:55 +0900

    モデルの単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name             { 'やマ田' }
+    first_name            { '太ろウ' }
+    kana_first_name       { 'ヤマダ' }
+    kana_last_name        { 'タロウ' }
+    birthday              { Faker::Date.birthday }
+  end
+end
